331267b4d3c4e98d16e784e4d65e48c75eb4314d
314Reactor <34923578+LordofBone@users.noreply.github.com>
2022-01-05 13:07:31 +0000

    Fixed bug that allowed a timed run to be kicked off when a manual refresh was running, potentially causing 2 neural transfer instances to be running at once

--- functions/timer_control.py
+++ functions/timer_control.py
@@ -21,12 +21,13 @@ TimerControl = Timer()
 def timer_image():
     while True:
         if TimerControl.timer_on:
-            TimerControl.running_display = True
-            refresh_run = BuildDisplay(show_image=False)
-            refresh_run.run_neural_transfer()
-            refresh_run.display_to_ink()
-            TimerControl.running_display = False
-            time.sleep(timer)
+            if not TimerControl.running_display:
+                TimerControl.running_display = True
+                refresh_run = BuildDisplay(show_image=False)
+                refresh_run.run_neural_transfer()
+                refresh_run.display_to_ink()
+                TimerControl.running_display = False
+                time.sleep(timer)
 
 
 t = threading.Thread(target=timer_image)
